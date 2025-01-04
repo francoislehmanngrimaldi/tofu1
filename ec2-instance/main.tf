eaef1da5cfc1606c78e7756d53e406309a30312e
Lehmann-Grimaldi <francois.lehmann-grimaldi@edu.esiee.fr>
2025-01-04 16:24:58 +0000

    Correction de l'indexation du security group

--- ec2-instance/main.tf
+++ ec2-instance/main.tf
@@ -10,14 +10,15 @@ resource "aws_security_group" "sample_app" {
 
 
 resource "aws_security_group_rule" "allow_http_inbound" {
-  type              = "ingress"
-  protocol          = "tcp"
+  count = length(aws_security_group.sample_app)
+
+  security_group_id = aws_security_group.sample_app[count.index].id
   from_port         = 8080
   to_port           = 8080
-  security_group_id = aws_security_group.sample_app[count.index].id
+  protocol          = "tcp"
   cidr_blocks       = ["0.0.0.0/0"]
-  count             = 2
 }
+
 
 resource "aws_instance" "sample_app" {
   ami                  = var.ami_id
